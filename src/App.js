a01b7899b3aed444801950e32415b1adda3b37b4
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-05-24 15:52:48 -0700

    setup react-app and add register component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Register from "./components/Register/Register";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // componentDidMount() {
+  //   this.getDogs();
+  // }
+  handleRegister = async data => {
+    try {
+      const registerCall = await fetch(
+        "http://localhost:8000/users/registration",
+        {
+          method: "POST",
+          body: JSON.stringify(data),
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      const response = await registerCall.json();
+      console.log(response, "from the flask server on localhost:8000");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // getDogs = async () => {
+  //   try {
+  //     const response = await fetch("http://localhost:8000/api/v1/dogs", {
+  //       credentials: "include"
+  //     });
+
+  //     if (response.ok) {
+  //       const responseParsed = await response.json();
+  //       console.log(responseParsed);
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+  render() {
+    return (
+      <div className="App">
+        <Register handleRegister={this.handleRegister} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class Register extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      verify_password: ""
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleRegister(this.state);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Username:
+          <input type="text" name="username" onChange={this.handleChange} />
+          <br />
+        </label>
+        <label>
+          email:
+          <input type="email" name="email" onChange={this.handleChange} />
+          <br />
+        </label>
+        <label>
+          password:
+          <input type="text" name="password" onChange={this.handleChange} />
+          <br />
+        </label>
+        <label>
+          verify_password:
+          <input
+            type="text"
+            name="verify_password"
+            onChange={this.handleChange}
+          />
+          <br />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    );
+  }
+}
+
+export default Register;
