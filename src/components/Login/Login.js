aceb9fd95863f5c3508ff9965714d420e1668c15
aBurmeseDev <47447266+aBurmeseDev@users.noreply.github.com>
2019-05-28 16:15:10 -0700

    add login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,65 +1,43 @@
-// import React, { Component } from "react";
-// import { withRouter } from "react-router-dom";
+import React, { Component } from "react";
 
-// class Login extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       username: "",
-//       password: ""
-//     };
-//   }
-//   //   handleChange = e => {
-//   //     this.setState({ [e.target.name]: e.target.value });
-//   //   };
-//   //   handleSubmit = async e => {
-//   //     e.preventDefault();
-//   //     const loginResponse = await fetch("/users/login", {
-//   //       method: "POST",
-//   //       credentials: "include",
-//   //       body: JSON.stringify(this.state),
-//   //       headers: {
-//   //         "Content-type": "application/json"
-//   //       }
-//   //     });
+class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleLogin(this.state);
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>Login</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <br />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <span>{this.props.loginMessage}</span>
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
 
-//   render() {
-//     console.log(this.state);
-//     return (
-//       <form onSubmit={this.handleSubmit} className="loginForm">
-//         <label
-//           htmlFor="username"
-//           style={{ color: "rgb(9, 93, 172)", fontSize: "1rem" }}
-//         >
-//           username
-//         </label>
-//         <input
-//           type="text"
-//           name="username"
-//           autoComplete="off"
-//           onChange={this.handleChange}
-//           style={{ color: "#ffffff" }}
-//         />
-//         <label
-//           htmlFor="password"
-//           style={{ color: "rgb(9, 93, 172)", fontSize: "1rem" }}
-//         >
-//           password
-//         </label>
-//         <input
-//           type="password"
-//           name="password"
-//           onChange={this.handleChange}
-//           autoComplete="off"
-//           style={{ color: "#ffffff" }}
-//         />
-//         <br />
-//         <button type="submit" className="btn waves-effect waves-light">
-//           Login
-//         </button>
-//       </form>
-//     );
-//   }
-// }
-
-// export default withRouter(Login);
+export default Login;
