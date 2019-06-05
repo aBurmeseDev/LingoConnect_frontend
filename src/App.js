bdff04ec96d53c93b8da21ac6dfd68172ea9114c
Jablan <joshuaablan@gmail.com>
2019-06-05 10:06:09 -0700

    remove console.log

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import AppNavBar from "./components/AppNavbar/AppNavbar";
 
 import * as routes from "./components/constants/routes";
 
-console.log(process.env)
 class App extends Component {
   state = {
     loginMessage: null,
